eec3d8b1c6afb876d6a3b31c3a26f2aaedde9ca0
Chris <dermentzoglou.chris@gmail.com>
2022-01-27 20:31:32 -0500

    COB non functional

--- main.js
+++ main.js
@@ -33,11 +33,6 @@ const ntClient = require('wpilib-nt-client');
 const client = new ntClient.Client();
 client.setReconnectDelay(1000);
 
-client.start((isConnected, err) => {
-    // Displays the error and the state of connection
-    console.log({ isConnected, err });
-}, 'localhost');
-
 // Adds a listener to the client
 client.addListener((key, val, type, id) => {
     console.log({ key, val, type, id });
