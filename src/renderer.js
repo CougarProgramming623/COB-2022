e282b479b0b9d31e94b224d25ac3d3e7c816b2c3
Carson Graham <carson42g@gmail.com>
2022-02-09 09:37:41 -0500

    saved file lol

--- src/renderer.js
+++ src/renderer.js
@@ -24,11 +24,7 @@ const COB_KEY = {
 
 
 function initAll(){
-    document.getElementById("compile").onclick = compileCPL;
-    document.getElementById("cpl-input").addEventListener("input", compileCPL);
-    document.getElementById("show-ast").onclick = function() {
-        document.getElementById("ast").hidden = !document.getElementById("ast").hidden;
-    }
+    initCPLUI();
     const field = document.getElementById("field");
     const fieldImg = document.getElementById("field-img");
     let fullscreen = false;
